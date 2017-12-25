e35fcb93595028c8fadbf653bb7b9a99a06ca38d
Chad <worktannoury@gmail.com>
2017-12-25 17:32:11 +0200

    Stage 1 Player Movment

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 60;
 };
 
 // Update the enemy's position, required method for game
@@ -21,10 +23,32 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+const enemy1 = new Enemy();
+let allEnemies = [enemy1];
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function() {
+    this.sprite = 'images/char-boy.png';
+};
+Player.prototype = Object.create(Enemy.prototype);
 
+let player = new Player();
+player.x = 202;
+player.y = 400;
+
+player.handleInput = function(key){
+    if(key === 'left' && player.x > 100){
+        player.x -= 100;
+    }else if(key === 'right' && player.x < 400){
+        player.x += 100;
+    }else if(key === 'up' && player.y > 0){
+        player.y -= 83;
+    }else if(key === 'down' && player.y < 400){
+        player.y += 83;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
