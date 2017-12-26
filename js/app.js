cdc70bcb833debd5f343548cbdaedeb2c87fb028
Chad <worktannoury@gmail.com>
2017-12-26 19:08:13 +0200

    Stage 3 Check Collision

--- js/app.js
+++ js/app.js
@@ -7,16 +7,15 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = -100;
-/*    var axe;
-    this.x = axe;
-    this.run = function(){
-        var speed = function(){
-            console.log(axe);
-            axe = axe + 2 ;
-            if(axe > 505){window.clearInterval(int2)};
+    this.collision = function(){
+        if(this.y === player.y){
+            if(player.x - 75 < this.x){
+                if(player.x + 70 > this.x){
+                    player.y = 396;
+                }
+            }
         }
-        var int2 = setInterval(speed, 10);
-    } */
+    }
 };
 
 // Update the enemy's position, required method for game
@@ -50,33 +49,33 @@ enemy3.speed = function(enemy){
 };
 
 var enemy4 = new Enemy();
-enemy4.y = 145;
+enemy4.y = 147;
 enemy4.speed = function(enemy){
     enemy.x = enemy.x + 1 ;
 };
 var enemy5 = new Enemy();
-enemy5.y = 145;
+enemy5.y = 147;
 enemy5.speed = function(enemy){
     enemy.x = enemy.x + 2 ;
 };
 var enemy6 = new Enemy();
-enemy6.y = 145;
+enemy6.y = 147;
 enemy6.speed = function(enemy){
     enemy.x = enemy.x + 3 ;
 };
 
 var enemy7 = new Enemy();
-enemy7.y = 60;
+enemy7.y = 64;
 enemy7.speed = function(enemy){
     enemy.x = enemy.x + 1 ;
 };
 var enemy8 = new Enemy();
-enemy8.y = 60;
+enemy8.y = 64;
 enemy8.speed = function(enemy){
     enemy.x = enemy.x + 2 ;
 };
 var enemy9 = new Enemy();
-enemy9.y = 60;
+enemy9.y = 64;
 enemy9.speed = function(enemy){
     enemy.x = enemy.x + 3 ;
 };
@@ -88,8 +87,8 @@ var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8
 var random = function(){
     var randEnemy = allEnemies[Math.floor(Math.random() * allEnemies.length)];
     var run = function(){   
-        console.log(randEnemy);
         randEnemy.speed(randEnemy);
+        randEnemy.collision();
         if(randEnemy.x > 505){
             window.clearInterval(int2)
             randEnemy.x = -100;
@@ -99,6 +98,10 @@ var random = function(){
 };
 setInterval(random, 500);
 
+var colision = function(){
+    if(Enemy)
+        console.log(Enemy);
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -109,7 +112,7 @@ Player.prototype = Object.create(Enemy.prototype);
 Player.prototype.construtor = Player;
 let player = new Player();
 player.x = 202;
-player.y = 400;
+player.y = 396;
 
 //Methode to handle player movment
 player.handleInput = function(key){
@@ -119,7 +122,7 @@ player.handleInput = function(key){
         player.x += 100;
     }else if(key === 'up' && player.y > 0){
         player.y -= 83;
-    }else if(key === 'down' && player.y < 400){
+    }else if(key === 'down' && player.y < 380){
         player.y += 83;
     }
 };
